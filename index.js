d7e93a4f21247f9e80925817415bf330e90ff761
NitishGP <gobindanitish13@gmail.com>
2024-08-17 09:07:16 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+require('dotenv').config();
+const paypal = require('./services/paypal')
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/',(req,res) => {
+    res.render('index');
+});
+
+app.post('/pay', async (req,res) => {
+    try{
+        const url = await paypal.createOrder()
+        res.redirect(url)
+    }catch(error){
+        res.send('Error: ' + error)
+    }
+})
+
+app.get('/complete-order',async (req,res) => {
+    try{
+        await paypal.captureOrder(req.query.token)
+        res.send("Thank you for your generous donation.")
+    }catch(error){
+        res.send('Error: ' + error)
+
+    }
+    
+})
+app.get('/cancel-order',(req,res) => {
+    res.redirect('/')
+})
+app.listen(3000, () => console.log("SERVER STARTED ON PORT 3000"));
